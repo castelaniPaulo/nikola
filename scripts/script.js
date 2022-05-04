de3ce1c75320b02c7a2740988ec48164d7d95500
castelaniPaulo <104774282+castelaniPaulo@users.noreply.github.com>
2022-05-04 10:59:08 -0300

    atualização

---
+++ scripts/script.js
@@ -0,0 +1,15 @@
+const nav=document.getElementsByTagName("nav")[0];
+const topoNav=nav.offsetTop;
+
+window.onscroll = function(){
+    fixarMenuNotopo();
+} 
+
+function fixarMenuNotopo(){
+    if(window.pageYOffset >= topoNav){
+        nav.classList.add("fixoNotopo")
+    }
+    else{
+        nav.classList.remove("fixoNotopo")
+    }
+}
